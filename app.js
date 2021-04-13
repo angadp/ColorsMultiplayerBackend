781232300e65518f83aa848395b5edd000d43195
Angad Nagpal <angadnagpal@Angads-MacBook-Pro.local>
2021-04-13 14:50:40 +0530

    [ Backend ] init

---
+++ app.js
@@ -0,0 +1,80 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//@ts-check
+const CosmosClient = require('@azure/cosmos').CosmosClient
+
+const config = require('./config')
+const url = require('url')
+
+const endpoint = config.endpoint
+const key = config.key
+
+const databaseId = config.database.id
+const containerId = config.container.id
+
+const client = new CosmosClient({ endpoint, key })
+
+/**
+ * Create the database if it does not exist
+ */
+async function createDatabase() {
+  const { database } = await client.databases.createIfNotExists({
+    id: databaseId
+  })
+  console.log(`Created database:\n${database.id}\n`)
+}
+
+async function createContainer(){
+  await client
+  .database(databaseId)
+  .containers.createIfNotExists(
+    { id: containerId },
+    { offerThroughput: 400 }
+  )
+}
+
+createDatabase().then(createContainer());
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.set('port', process.env.PORT || 8080);
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(8080, '0.0.0.0');
+
+module.exports = app;
---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {}
+
+config.endpoint = 'https://colors-multiplayer.documents.azure.com:443/'
+config.key = 'Dv4rmCEP0Xepqs6Bfw8XPkWCJQqNuomoRLDNAq061P4lD1jqdO0iMbkIFr1dqNrQwrRVmRdTTFGvbXitdiwn2w=='
+
+config.database = {
+  id: 'ColorsMultiplayer'
+}
+
+config.container = {
+  id: 'Game'
+}
+
+module.exports = config
