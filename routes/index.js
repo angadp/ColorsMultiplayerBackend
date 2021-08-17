de70f0a51eb6a87a3a091a5e50bb88daabf32469
Angadpreet Nagpal <angadpreet.nagpal@gmail.com>
2021-08-17 16:46:16 +0530

    [ Add logic for getting and adding board ]

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
 var express = require('express')
 var router = express()
 
-router.get('/', (req, res) => {
-    console.log('here');
-    res.send('Hello');
+const CosmosClient = require('@azure/cosmos').CosmosClient
+
+const config = require('../config')
+const url = require('url')
+
+const endpoint = config.endpoint
+const key = config.key
+
+const databaseId = config.database.id
+const containerId = config.container.id
+
+const client = new CosmosClient({ endpoint, key })
+
+const database = client.database(databaseId);
+
+const container = database.container(containerId);
+
+router.get('/', async (req, res) => {
+    if(req.query['BoardId'] != undefined) {
+        const querySpec = {
+            query: 'SELECT * from c WHERE c.BoardId="' + req.query['BoardId']+'"' 
+        };
+
+        console.log(querySpec);
+        
+        const { resources: items } = await container.items
+            .query(querySpec)
+            .fetchAll();
+
+            res.send(JSON.stringify({
+                board: items[0].board
+            }));
+    } else {
+        var board = [];
+        for(var i=0;i<9;i++){
+            board.push([]);
+            for(var j=0;j<9;j++){
+                board[i].push(Math.floor((Math.random() * 10)));
+            }
+        }
+        const id = new Date().toJSON().replace(/-/g, '').replace(/:/g, '');
+        const newItem = {
+            BoardId: id,
+            board: board
+        };
+        const { resource: createdItem } = 
+            await container.items.create(newItem)
+                .then(response => {
+                    res.send(JSON.stringify({
+                        board: board
+                    }));
+                }).catch(ex =>{ console.log(ex)});
+    }
+    
 })
 
 module.exports = router;
